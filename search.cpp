b2d8fe68e04620328d700223a89fcad8ce4c2b6f
ltr6134439 <634846185@qq.com>
2015-04-20 23:54:54 +0800

    search

---
+++ search.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file search.cpp
+ * @author liutongren(liutongren@baidu.com)
+ * @date 2015/04/20 23:47:22
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
